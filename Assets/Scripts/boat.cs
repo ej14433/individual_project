d2521fa64e91ef95d7e1c9f3577a60c476284198
Elis Jones <ej14433@my.bristol.ac.uk>
2018-02-20 12:51:28 +0000

    Textured, and detail on ILB model

--- Assets/Scripts/boat.cs
+++ Assets/Scripts/boat.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class boat : MonoBehaviour {
 
 	private Rigidbody rbody;
-	public float turnSpeed = 1000f;
-	public float accSpeed = 1000f;
-	public float prop_speed = 200f;
-	public float tilt = 20f;
-	public float bank = 20f;
+	public float turnSpeed = 1000.0f;
+	public float accSpeed = 1000.0f;
+	public float prop_speed = 200.0f;
+	public float tilt = 20.0f;
+	public float bank = 20.0f;
 
 	public GameObject engine;
 	public GameObject boat_obj;
@@ -29,16 +29,17 @@ public class boat : MonoBehaviour {
 		float v = Input.GetAxis ("Vertical");
 
 		propellers.transform.eulerAngles = new Vector3 (0.0f, engine.transform.eulerAngles.y, propellers.transform.eulerAngles.z + v * prop_speed);
-		engine.transform.eulerAngles = new Vector3 (0.0f, boat_obj.transform.eulerAngles.y - h * 45.0f, boat_obj.transform.eulerAngles.z);
+		engine.transform.eulerAngles = new Vector3 (boat_obj.transform.eulerAngles.x, boat_obj.transform.eulerAngles.y + 180.0f - h * 45.0f, boat_obj.transform.eulerAngles.z);
 		if (v >= 0) {
-			boat_obj.transform.eulerAngles = new Vector3 (-v * tilt, boat_obj.transform.eulerAngles.y, -h * bank);
+			boat_obj.transform.eulerAngles = new Vector3 (-v * tilt, boat_obj.transform.eulerAngles.y, -h * v * bank);
 		}
 		//boat_obj.transform.eulerAngles = new Vector3 (0f, boat_obj.transform.eulerAngles.y, 0f);
-		rbody.AddTorque (0f, h * v * turnSpeed * Time.deltaTime, 0f);
+		rbody.AddTorque (0f, h * v * turnSpeed * Time.deltaTime, 0.0f);
 
 		rbody.AddForce (transform.forward * accSpeed * v * Time.deltaTime);
 
-		boat_obj.transform.position = new Vector3 (boat_obj.transform.position.x, 0f, boat_obj.transform.position.z);
+		boat_obj.transform.position = new Vector3 (boat_obj.transform.position.x, 1.0f, boat_obj.transform.position.z);
+		engine.transform.eulerAngles = new Vector3 (-boat_obj.transform.eulerAngles.x, boat_obj.transform.eulerAngles.y + 180.0f - h * 45.0f, boat_obj.transform.eulerAngles.z);
 
 	}
 }
